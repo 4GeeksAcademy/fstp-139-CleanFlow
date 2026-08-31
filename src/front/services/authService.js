72fa0f5980c8e02726120c97f09db23b810f3e19
mateore30 <268670521+mateore1330-commits@users.noreply.github.com>
2026-08-31 21:04:47 +0000

    feature/login "create authService.js"

---
+++ src/front/services/authService.js
@@ -0,0 +1,19 @@
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+
+export const login = async (email, password) => {
+    const response = await fetch(`${BACKEND_URL}/api/login`, {
+        method: "POST",
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    const data = await response.json();
+    return {
+        ok: response.ok,
+        data,
+    };
+};
