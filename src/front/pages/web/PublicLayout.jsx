45b1a9ebf92b8465c4ea75f8859f76370d4d4f7c
mateore30 <268670521+mateore1330-commits@users.noreply.github.com>
2026-09-07 22:14:21 +0000

    feat/navbar-footer "add import web.css"

--- src/front/pages/web/PublicLayout.jsx
+++ src/front/pages/web/PublicLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom"
 import ScrollToTop from "../../components/web/ScrollToTop"
 import { Navbar } from "../../components/web/Navbar"
 import { Footer } from "../../components/web/Footer"
+import "../../web.css"
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const PublicLayout = () => {
